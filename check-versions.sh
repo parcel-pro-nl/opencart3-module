1292c7c9978014d10f8ec34a5c1213856d865bbf
Luca Scalzotto <luca@scorpiac.com>
2023-11-15 16:27:04 +0100

    Add install.xml to version check, update install.xml version

--- check-versions.sh
+++ check-versions.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 allMatch=1
 function checkVersionMatch() {
     echo "- $1: $2"
-    if [ ! "$changelogVersion" = "$2" ]
+    if [ ! "$installXmlVersion" = "$2" ]
     then
       allMatch=0
     fi
@@ -12,9 +12,10 @@ function checkVersionMatch() {
 
 echo "Detected versions:"
 
-changelogVersion="$(sed -nE 's/^## V(.*)$/\1/p' 'CHANGELOG.md' | head -n 1)"
-checkVersionMatch 'CHANGELOG.md' "$changelogVersion"
+installXmlVersion="$(sed -nE 's/^.*version.*\[(.*)\]\].*$/\1/p' 'install.xml')"
+checkVersionMatch 'install.xml' "$installXmlVersion"
 
+checkVersionMatch 'CHANGELOG.md' "$(sed -nE 's/^## V(.*)$/\1/p' 'CHANGELOG.md' | head -n 1)"
 checkVersionMatch 'upload/admin/language/dutch/sale/pp_order.php' "$(sed -nE "s/^.version = '(.*)'.*/\1/p" 'upload/admin/language/dutch/sale/pp_order.php')"
 checkVersionMatch 'upload/admin/language/en-gb/sale/pp_order.php' "$(sed -nE "s/^.version = '(.*)'.*/\1/p" 'upload/admin/language/en-gb/sale/pp_order.php')"
 checkVersionMatch 'upload/admin/language/dutch/extension/shipping/parcel_pro.php' "$(sed -nE "s/^.version = '(.*)'.*/\1/p" 'upload/admin/language/dutch/extension/shipping/parcel_pro.php')"
